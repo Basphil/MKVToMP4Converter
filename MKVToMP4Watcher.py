98c36200b744e3b936f529ab7335e7a3cdc78d24
Basil Philipp <basil.philipp@gmail.com>
2011-04-22 15:10:31 +0200

    Now only reacts to IN_CLOSE_WRITE event

--- MKVToMP4Watcher.py
+++ MKVToMP4Watcher.py
@@ -1,30 +1,23 @@
 import pyinotify 
-import MKVtoMP4Converter as converter
+import MKVToMP4Converter as converter
 
 wm = pyinotify.WatchManager()
 
-mask = pyinotify.IN_MOVED_TO | pyinotify.IN_CREATE# watched events
+flags = pyinotify.IN_CLOSE_WRITE
 
 class EventHandler(pyinotify.ProcessEvent):
     
-    def process_IN_MOVED_TO(self, event):
-        print "Moved:"+event.pathname
+    def process_IN_CLOSE_WRITE(self, event):
+        print "Finished writing:"+event.pathname
         if event.pathname[-3:]=='mkv':
             print 'Starting conversion of %s' % event.pathname
             converter.convert(event.pathname)
-            print 'Done.'
-            
-            
-    def process_IN_CREATE(self, event):
-        print "Created:"+event.pathname
-    
+            print 'Done.' 
     
     
 handler = EventHandler()
 notifier = pyinotify.Notifier(wm, handler)
 
-wdd = wm.add_watch('/media/mediadisk/files', mask, rec=True, auto_add=True)
+wdd = wm.add_watch('/media/mediadisk/files', flags, rec=True, auto_add=True)
 notifier.loop()
-
-
 
