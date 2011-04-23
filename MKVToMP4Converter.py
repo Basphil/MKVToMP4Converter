6ee03a2c72bfe731cd361d245d69285603ac223d
Basil Philipp <basil.philipp@gmail.com>
2011-04-23 20:04:30 +0200

    Small improvements and added GPLv3 disclaimer.

--- MKVToMP4Converter.py
+++ MKVToMP4Converter.py
@@ -1,3 +1,24 @@
+## MKVToMP4Converter: Programm that uses a set of existing tools to convert
+## .mkv files into .mp4 files.
+
+## Source code and additional information at 
+## https://github.com/Basphil/MKVToMP4Converter
+
+## Copyright (C) 2011  Basil Philipp
+
+## This program is free software: you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation, either version 3 of the License, or
+## (at your option) any later version.
+
+## This program is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+
+## You should have received a copy of the GNU General Public License
+## along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 import subprocess,shlex, string, sys
 
 def convert(path):    
--- MKVToMP4Watcher.py
+++ MKVToMP4Watcher.py
@@ -1,3 +1,25 @@
+## MKVToMP4Watcher: Programm that uses pyinotify to run MKVToMP4Converter 
+## on .mkv files moved to, or created in the watched directory.
+
+## Source code and additional information at 
+## https://github.com/Basphil/MKVToMP4Converter
+
+## Copyright (C) 2011  Basil Philipp
+
+## This program is free software: you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation, either version 3 of the License, or
+## (at your option) any later version.
+
+## This program is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+
+## You should have received a copy of the GNU General Public License
+## along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
 import pyinotify 
 import MKVToMP4Converter as converter
 import sys
